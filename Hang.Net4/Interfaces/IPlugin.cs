7c79fcdd402770fe69db1983daaf200c7504c1ca
Hang <yhnbgfd@gmail.com>
2016-05-07 02:19:19 +0800

    ===

---
+++ Hang.Net4/Interfaces/IPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Hang.Net4.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hang.Net4.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Tuple<PluginType, object> Register();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        bool UnRegister();
+    }
+}
